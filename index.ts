1b6a0114d87cc58b7341cb13695d8e8945be7aae
Frzct <gusiccat@gmail.com>
2024-09-23 20:41:44 +0700

    index.ts changed

--- index.ts
+++ index.ts
@@ -1,6 +1,15 @@
-const express = require("express")
-const Application = express()
+import express from "express"
+import Path from "path"
 
-Application.get("3000", () => {
+const Application = express()
+const PublicPath = Path.join("./public")
+
+express.static(PublicPath)
+
+Application.get("/", (req, res) => {
+    res.send("./public/landing/index.html")
+})
+
+Application.listen("3000", () => {
     console.log("Running i guess....")
 })
