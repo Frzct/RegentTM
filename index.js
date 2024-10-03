cbad77b2ee91800bded73773d8ac72a87a9ce01f
Frzct <gusiccat@gmail.com>
2024-10-03 11:07:17 +0700

    the thing works

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-var express = require('express')
-var app = express()
+const Express = require('express')
+const Application = Express()
 
-app.use(express.static("public"));
+//
 
-app.get("/", (req, res) => {
-    res.send("hello")
+var _mainPath = require("path")
+var Public = _mainPath.join(__dirname, "public")
+
+//
+var List = {
+    client: _mainPath.join( Public, "client" ),
+    landing: _mainPath.join( Public, "landing" ),
+}
+
+//
+
+Application.use(Express.static(Public))
+
+Application.get("/", (req, res) => {
+    res.sendFile(_mainPath.join( List.landing, "index.html") )
 })
 
-app.listen("3000", () => {
+Application.listen("3000", () => {
     console.log("Running i guess....")
 })
