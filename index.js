5621163649bd722918adc0738f3b4ace885ad5ee
Frzct <gusiccat@gmail.com>
2024-10-03 11:10:58 +0700

    Cleaned up the _mainPath.join() spam

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const Application = Express()
 
 //
 
-var _mainPath = require("path")
-var Public = _mainPath.join(__dirname, "public")
+var _mainPath = require("path") // Note: Running require on "path" will not return your __dirname
+const quick_join = _mainPath.join
+
+var Public = quick_join(__dirname, "public") //... so you gotta merge it like this.
 
 //
 var List = {
-    client: _mainPath.join( Public, "client" ),
-    landing: _mainPath.join( Public, "landing" ),
+    client: quick_join( Public, "client" ),
+    landing: quick_join( Public, "landing" ),
 }
 
 //
@@ -17,7 +19,7 @@ var List = {
 Application.use(Express.static(Public))
 
 Application.get("/", (req, res) => {
-    res.sendFile(_mainPath.join( List.landing, "index.html") )
+    res.sendFile( quick_join( List.landing, "index.html") )
 })
 
 Application.listen("3000", () => {
