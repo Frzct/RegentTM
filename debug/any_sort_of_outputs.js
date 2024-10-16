82bf1072096b6043e116f488282209bdece8bd68
Frzct <gusiccat@gmail.com>
2024-10-16 20:29:55 +0700

    debug folder

---
+++ debug/any_sort_of_outputs.js
@@ -0,0 +1,11 @@
+
+export class output {
+    ___basicFormat = () => {};
+    log = (...msgs) => {
+        console.log('hi this workedd')
+    };
+    warn = (...msgs) => {};
+    error = (...msgs) => {};
+}
+
+// for now it's broken
