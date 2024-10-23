79228e388c481705861549b4c2e20fd86fb5e3e5
Frzct <gusiccat@gmail.com>
2024-10-23 15:27:52 +0700

    bruh

--- public/landing/index.js
+++ public/landing/index.js
@@ -1,10 +1,23 @@
-let OutOfDocument = false
-let id_save_documents = {
-    "button-1": ["id", "addEventListener", () => {
-        
+var OutOfDocument = false
+var id_save_documents = {
+    "button-1": ["addEventListener", "click", () => {
+        console.log("you clicked")
+        window.location = "/study_tab/index.html"
     }],
 
 }
+
+document.getElementById("button-1").addEventListener("onclick", (ev) => {
+    console.log("communism")
+    window.location = "https://youtube.com"
+})
+
+Object.keys(id_save_documents).forEach((key) => {
+    let id_save = id_save_documents[key]
+    console.log("cool id save")
+    document.getElementById(key)[id_save[0]](id_save[1], id_save[2])
+})
+
 window.addEventListener("visibilitychange", (event) => {
     OutOfDocument = !OutOfDocument
 
