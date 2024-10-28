85575beae5afee16c1ee9a85280ce16a8bba2d4d
Frzct <gusiccat@gmail.com>
2024-10-28 08:57:33 +0700

    finally

--- public/landing/index.js
+++ public/landing/index.js
@@ -23,9 +23,24 @@ window.addEventListener("visibilitychange", (event) => {
     OutOfDocument = !OutOfDocument
 
     if (!OutOfDocument){
-        var diff = Date.now() - hopOffTime
+        let diff = Date.now() - hopOffTime
+        let Time_display = {
+            "Hours": Math.floor(diff/3600000),
+            "Minutes": Math.floor(diff/60000 % 60),
+            "Seconds": Math.floor(diff/1000 % 60)
+        }
+        let finalText = ""
+        let KeyList = Object.keys(Time_display)
         
-        alert(`Welcome back, user. you have been offline for ${Math.floor(diff/60000)} maniutes and ${Math.floor(diff/1000)} sekonds!!!!`)
+        for (i = 0; i < KeyList.length; i++){
+            let K = KeyList[i]
+            let Condition = i < (KeyList.length - 1)
+            //
+            finalText += `${!Condition ? "and ": ""}${Time_display[K]} ${K}${Condition ? ", ": ""}`
+        }
+        document.getElementById("offline-time").style = "display: block;"
+
+        document.getElementById("offline-time-text").innerText = finalText
     } else {
         hopOffTime = Date.now()
     }
